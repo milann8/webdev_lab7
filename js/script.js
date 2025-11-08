985870f26950e3c63442e5f75474906f7c566de3
Milan Wong <121139048+milann8@users.noreply.github.com>
2025-11-07 18:38:39 -0600

    cha

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+    function myEventFunct(){
+        var message = document.getElementById('entry_input').value;
+        alert("Milan: " + message);
+        document.getElementById('result').innerHTML = "<h3>" + message + "</h3>";
+    }
+    var result= document.getElementById('entrybutton');
+    result.addEventListener('click',myEventFunct)
+}
 window.addEventListener('load', init);
+
+
